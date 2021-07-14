48872474483cbb748ae569a26d122c5946b73b59
mguppy <mguppy34@gmail.com>
2021-07-14 14:13:29 -0500

    added notes from class

---
+++ index.js
@@ -0,0 +1,27 @@
+// TODO: Include packages needed for this application
+// require generateMarkdown
+// require inquirer
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    //
+}];
+
+//When I want the above to work, you can call inquirer.prompt(questions).then
+
+.then((responses) => {
+    //Need to pass responses into generateMarkdown
+    
+}
+
+);
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
