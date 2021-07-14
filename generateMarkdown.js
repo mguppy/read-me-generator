0ab92e51957a43f3e20a0c8ef2521713178f8e8d
mguppy <mguppy34@gmail.com>
2021-07-14 14:20:42 -0500

    npm inquirer added

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -15,6 +17,8 @@ function renderLicenseSection(license) {}
 function generateMarkdown(responses) {
   return `# ${responses.title}
   ## ${responses.name}
+
+  ${renderLicenseBadge(responses.license)}
 `;
 }
 
